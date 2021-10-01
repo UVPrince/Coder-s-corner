737a6c0c3ec32b26587e0a021ef048fb41feccbb
Yuvraj <90333311+UVPrince@users.noreply.github.com>
2021-10-01 06:44:51 +0530

    Create ask.py

---
+++ ask.py
@@ -0,0 +1,8 @@
+a=['amit','aman','abishek','amar']
+b=input("enter your name: ")
+if b.lower() in a:
+  print(b,", Welcome to the party")
+  print("You were invited")
+  
+else:
+  print("better luck next time")
